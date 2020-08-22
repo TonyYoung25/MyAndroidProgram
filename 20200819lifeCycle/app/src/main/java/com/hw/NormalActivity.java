7750638df377d97dbdbc60f36ec1c006ff4dbac7
yangpengfei <yangpengfei25@126.com>
2020-08-22 22:00:56 +0800

    测试git

--- 20200819lifeCycle/app/src/main/java/com/hw/NormalActivity.java
+++ 20200819lifeCycle/app/src/main/java/com/hw/NormalActivity.java
@@ -10,5 +10,6 @@ public class NormalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal);
+        //测试git
     }
 }
