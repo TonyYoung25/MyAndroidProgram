06dc884c61c6595ad41bb539c3fcce2aaf68412e
yangpengfei <yangpengfei25@126.com>
2020-08-30 23:15:41 +0800

    demo

--- 20200823FileStorage/app/src/main/java/com/hw/MainActivity.java
+++ 20200823FileStorage/app/src/main/java/com/hw/MainActivity.java
@@ -2,13 +2,93 @@ package com.hw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+
+        String text = load();
+        if(!TextUtils.isEmpty(text)){
+            editText.setText(text);
+            editText.setSelection(text.length());//光标调到最后
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        String text = editText.getText().toString();
+        save(text);
+    }
+
+    //保存
+    private void save(String text) {
+
+        FileOutputStream fileOutputStream = null;
+        BufferedWriter bufferedWriter = null;
+
+        try {
+            fileOutputStream = openFileOutput("data", Context.MODE_PRIVATE);
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(fileOutputStream));
+            bufferedWriter.write(text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bufferedWriter != null) {
+                    bufferedWriter.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //加载
+    private String load() {
+        FileInputStream fileInputStream = null;
+        BufferedReader bufferedReader = null;
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            fileInputStream = openFileInput("data");
+            bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+            String line = "";//一行数据
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try{
+                if(bufferedReader != null){
+                    bufferedReader.close();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        return sb.toString();
     }
 }
