0c9ccd404749815e53b55aa4c4f9e28535ab2319
yangpengfei <yangpengfei25@126.com>
2020-09-09 07:36:09 +0800

    db

---
+++ 20200909DbActivity/app/src/main/java/com/hw/SharePreferencesActivity.java
@@ -0,0 +1,16 @@
+package com.hw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SharePreferencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_preferences);
+
+
+    }
+}
